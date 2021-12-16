bc996a6ed735ba5b10761d74e8f3e9dcaed6bef5
tanvy1 <trai6696@taehs.org>
2021-12-15 21:17:42 -0500

    commit

---
+++ navigator/TabNavigator.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "../screens/HomeScreen";
+import SectionScreen from "../screens/SectionScreen";
+
+
+const HomeStack= createStackNavigator({
+  Home: HomeScreen,
+  Section: SectionScreen,
+});
+      
+const CoursesStack = createStackNavigator({
+  Courses: SectionScreen
+});
+
+const ProjectsStack = createStackNavigator({
+  Projects:SectionScreen
+});
+
+const TabNavigator = createBottomTabNavigator({
+  HomeStack,
+  CoursesStack,
+  ProjectsStack
+});
+
+export default TabNavigator;
